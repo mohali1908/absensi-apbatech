ee41bc898e64d7d71f22e876c902d88e3928ba63
Mohammad Ali <mohammad.ali1908@gmail.com>
2024-10-15 17:01:23 +0700

    set default jumlah cuti kondisi

--- app/Http/Livewire/LeaveCreateForm.php
+++ app/Http/Livewire/LeaveCreateForm.php
@@ -20,7 +20,7 @@ class LeaveCreateForm extends Component
     public Collection $leavetypes;
     public Collection $accept;
     public Collection $users;
-    
+
     public function mount()
     {
         $this->attendances = Attendance::all();
@@ -45,7 +45,8 @@ class LeaveCreateForm extends Component
             'remaining_days_off' => Carbon::today()->toDateString(), 
             'reason' => '', 
             'is_accepted' => '',
-            'attendance_id' => ''
+            'attendance_id' => '',
+            'half_day' => '' // Tambahkan untuk radio button siang/pagi
         ];
     }
 
@@ -61,7 +62,8 @@ class LeaveCreateForm extends Component
             'remaining_days_off' => '', 
             'reason' => '', 
             'is_accepted' => '',
-            'attendance_id' => ''
+            'attendance_id' => '',
+            'half_day' => '' // Tambahkan untuk radio button siang/pagi
         ];
     }
 
@@ -79,12 +81,10 @@ class LeaveCreateForm extends Component
             $index = explode('.', $propertyName)[1]; // Ambil index dari leaves
             $userId = $this->leaves[$index]['user_id'];
 
-            
-            
             $lastLeave = Leave::where('user_id', $userId)
-            ->orderBy('end_date', 'desc') // Urutkan berdasarkan end_date terbaru
-            ->first();
-            
+                ->orderBy('end_date', 'desc') // Urutkan berdasarkan end_date terbaru
+                ->first();
+
             if ($lastLeave) {
                 // Jika ada data cuti sebelumnya, ambil sisa cuti dari record tersebut
                 $this->leaves[$index]['remaining_days_off'] = $lastLeave->remaining_days_off;
@@ -97,15 +97,9 @@ class LeaveCreateForm extends Component
 
     public function calculateEndDate($startDate, $numberOfLeaves)
     {
-        // Parse tanggal mulai menggunakan Carbon
         $startDate = \Carbon\Carbon::parse($startDate);
-        
-        // Variabel untuk menyimpan tanggal akhir
         $endDate = $startDate->copy();
-        
-        // Hitung tanggal akhir dengan mengabaikan akhir pekan
         while ($numberOfLeaves > 0) {
-            // Abaikan hari Sabtu dan Minggu
             if ($endDate->isWeekday()) {
                 $numberOfLeaves--;
             }
@@ -113,104 +107,85 @@ class LeaveCreateForm extends Component
                 $endDate->addDay();
             }
         }
-        
-        // Kembalikan tanggal akhir
         return $endDate->toDateString();
     }
 
-    // Validasi dan simpan data cuti
     public function saveLeaves()
     {
-    // Validasi input terlebih dahulu
-    $acceptIdRuleIn = join(',', $this->accept->pluck('id')->toArray());
-    $leavetypeIdRuleIn = join(',', $this->leavetypes->pluck('id')->toArray());
+        $acceptIdRuleIn = join(',', $this->accept->pluck('id')->toArray());
+        $leavetypeIdRuleIn = join(',', $this->leavetypes->pluck('id')->toArray());
 
-
-
-    $this->validate([
-        'leaves.*.user_id' => 'required',
-        'leaves.*.leave_type' => 'required|in:' . $leavetypeIdRuleIn,
-        'leaves.*.number_of_leaves' => 'required|numeric|min:1',
-        'leaves.*.remaining_days_off' => 'required|numeric',
-        'leaves.*.reason' => 'required',
-        'leaves.*.is_accepted' => 'required|in:' . $acceptIdRuleIn,
-    ]);
-
-    $affected = 0;
-    foreach ($this->leaves as $leave) {
-        $userId = $leave['user_id'];
-        $startDate = $leave['start_date'];
-        $numberOfLeaves = (int)$leave['number_of_leaves'];
-        $leaveType = (int)$leave['leave_type'];
-
-        // Validasi manual
-        if ($leaveType == 2 && $numberOfLeaves != 1) {
-            $this->addError('Untuk jenis cuti 2, jumlah cuti harus 1 hari.');
-            continue;
-        }
-
-        // Kalkulasi tanggal akhir
-        $endDate = $this->calculateEndDate($startDate, $numberOfLeaves);
-
-        // Ambil data cuti terakhir berdasarkan tanggal akhir cuti
-        $lastLeave = Leave::where('user_id', $userId)
-            ->orderBy('end_date', 'desc')
-            ->first();
-
-        $lastRemainingDaysOff = $lastLeave ? $lastLeave->remaining_days_off : 0;
-
-        // Sesuaikan pengurangan sisa cuti berdasarkan leave_type
-        $leaveDaysToDeduct = $leaveType == 2 ? $numberOfLeaves * 0.5 : $numberOfLeaves;
-
-        $calculatedRemainingDaysOff = $lastRemainingDaysOff - $leaveDaysToDeduct;
-
-        // Jika sisa cuti terakhir adalah 0 atau kurang, gunakan nilai dari input dan kurangi dengan jumlah cuti
-        if ($calculatedRemainingDaysOff < 0) {
-            $calculatedRemainingDaysOff = $leave['remaining_days_off'] - $leaveDaysToDeduct;
-        }
-
-        
-
-
-        // Simpan data cuti ke dalam tabel leaves
-        Leave::create([
-            'user_id' => $leave['user_id'],
-            'leave_type' => $leave['leave_type'],
-            'start_date' => $startDate,
-            'end_date' => $endDate,
-            'number_of_leaves' => $numberOfLeaves,
-            'remaining_days_off' => $calculatedRemainingDaysOff,
-            'reason' => $leave['reason'],
-            'is_leave' => $leave['is_accepted'],
+        $this->validate([
+            'leaves.*.user_id' => 'required',
+            'leaves.*.leave_type' => 'required|in:' . $leavetypeIdRuleIn,
+            'leaves.*.number_of_leaves' => 'required|numeric|min:1',
+            'leaves.*.remaining_days_off' => 'required|numeric',
+            'leaves.*.reason' => 'required',
+            'leaves.*.is_accepted' => 'required|in:' . $acceptIdRuleIn,
         ]);
 
-        // Simpan data presences
-        $currentDate = \Carbon\Carbon::parse($startDate);
-        $leaveDates = [];
-        while ($numberOfLeaves > 0) {
-            if ($currentDate->isWeekday()) {
-                $leaveDates[] = $currentDate->toDateString();
-                $numberOfLeaves--;
+        $affected = 0;
+        foreach ($this->leaves as $leave) {
+            $userId = $leave['user_id'];
+            $startDate = $leave['start_date'];
+            $numberOfLeaves = (int)$leave['number_of_leaves'];
+            $leaveType = (int)$leave['leave_type'];
+
+            if ($leaveType == 2 && $numberOfLeaves != 1) {
+                $this->addError('Untuk jenis cuti 2, jumlah cuti harus 1 hari.');
+                continue;
             }
-            $currentDate->addDay();
-        }
 
-        foreach ($leaveDates as $date) {
-            Presence::create([
+            $endDate = $this->calculateEndDate($startDate, $numberOfLeaves);
+
+            $lastLeave = Leave::where('user_id', $userId)
+                ->orderBy('end_date', 'desc')
+                ->first();
+
+            $lastRemainingDaysOff = $lastLeave ? $lastLeave->remaining_days_off : 0;
+            $leaveDaysToDeduct = $leaveType == 2 ? $numberOfLeaves * 0.5 : $numberOfLeaves;
+            $calculatedRemainingDaysOff = $lastRemainingDaysOff - $leaveDaysToDeduct;
+
+            if ($calculatedRemainingDaysOff < 0) {
+                $calculatedRemainingDaysOff = $leave['remaining_days_off'] - $leaveDaysToDeduct;
+            }
+
+            Leave::create([
                 'user_id' => $leave['user_id'],
-                'attendance_id' => $leave['attendance_id'] ?? 1, // Default ke 1 jika tidak diisi
-                'presence_date' => $date,
-                'is_leave' => 1,
-                'presence_from' => 0,
-                'presence_enter_time' => $leave['leave_type'] == 2 ? now()->toTimeString() : null,
+                'leave_type' => $leave['leave_type'],
+                'start_date' => $startDate,
+                'end_date' => $endDate,
+                'number_of_leaves' => $numberOfLeaves,
+                'remaining_days_off' => $calculatedRemainingDaysOff,
+                'reason' => $leave['reason'],
+                'is_leave' => $leave['is_accepted'],
             ]);
+
+            $currentDate = \Carbon\Carbon::parse($startDate);
+            $leaveDates = [];
+            while ($numberOfLeaves > 0) {
+                if ($currentDate->isWeekday()) {
+                    $leaveDates[] = $currentDate->toDateString();
+                    $numberOfLeaves--;
+                }
+                $currentDate->addDay();
+            }
+
+            foreach ($leaveDates as $date) {
+                Presence::create([
+                    'user_id' => $leave['user_id'],
+                    'attendance_id' => $leave['attendance_id'] ?? 1,
+                    'presence_date' => $date,
+                    'is_leave' => 1,
+                    'presence_from' => 0,
+                    'presence_enter_time' => $leave['leave_type'] == 2 ? now()->toTimeString() : null,
+                ]);
+            }
+
+            $affected++;
         }
 
-        $affected++;
-    }
-
-    // Redirect atau memberikan notifikasi setelah berhasil
-    redirect()->route('leaves.index')->with('success', "Ada ($affected) data cuti karyawan yang berhasil ditambahkan.");
+        redirect()->route('leaves.index')->with('success', "Ada ($affected) data cuti karyawan yang berhasil ditambahkan.");
     }
 
     public function render()
@@ -218,3 +193,4 @@ class LeaveCreateForm extends Component
         return view('livewire.leave-create-form');
     }
 }
+
--- resources/views/livewire/leave-create-form.blade.php
+++ resources/views/livewire/leave-create-form.blade.php
@@ -2,6 +2,7 @@
     <form wire:submit.prevent="saveLeaves" method="post">
         @foreach ($leaves as $i => $leave)
         <div class="mb-3">
+            <!-- Nama Karyawan -->
             <div>
                 <label for="user_id{{ $i }}">Nama Karyawan {{ $i + 1 }}</label>
                 <select class="form-select" wire:model="leaves.{{ $i }}.user_id">
@@ -12,18 +13,20 @@
                 </select>
                 @error('leaves.' . $i . '.user_id') <span class="text-danger">{{ $message }}</span> @enderror
             </div>
-            <div class="mb-3">               
+            
+            <!-- Jenis Absensi -->
+            <div class="mb-3">
                 <x-form-label id="attendance_id{{ $i }}" label='Jenis Absensi {{ $i + 1 }}' />
-                <select class="form-select" aria-label="Default select example" name="attendance_id"
-                    wire:model.defer="leaves.{{ $i }}.attendance_id">
+                <select class="form-select" aria-label="Default select example" wire:model.defer="leaves.{{ $i }}.attendance_id">
                     <option selected>-- Pilih Absensi --</option>
-                    @foreach ($attendances as $attendance)                        
+                    @foreach ($attendances as $attendance)
                     <option value="{{ $attendance->id }}">{{ ucfirst($attendance->title) }}</option>
                     @endforeach
                 </select>
                 <x-form-error key="leaves.{{ $i }}.attendance_id" />
             </div>
 
+            <!-- Jenis Cuti -->
             <div>
                 <label for="leave_type{{ $i }}">Jenis Cuti {{ $i + 1 }}</label>
                 <select class="form-select" wire:model="leaves.{{ $i }}.leave_type">
@@ -34,35 +37,55 @@
                 </select>
                 @error('leaves.' . $i . '.leave_type') <span class="text-danger">{{ $message }}</span> @enderror
             </div>
+
+            <!-- Kondisi Half Cuti -->
+            @if ($leave['leave_type'] == 'half') <!-- assuming 'half' is the ID for half cuti -->
+            <div class="mt-3">
+                <label>Half Cuti - Pilih Waktu</label>
+                <div>
+                    <input type="radio" id="pagi{{ $i }}" value="pagi" wire:model.defer="leaves.{{ $i }}.half_day_type">
+                    <label for="pagi{{ $i }}">Pagi</label>
+                </div>
+                <div>
+                    <input type="radio" id="siang{{ $i }}" value="siang" wire:model.defer="leaves.{{ $i }}.half_day_type">
+                    <label for="siang{{ $i }}">Siang</label>
+                </div>
+                @error('leaves.' . $i . '.half_day_type') <span class="text-danger">{{ $message }}</span> @enderror
+            </div>
+            @endif
+
+            <!-- Kondisi Full Cuti -->
+            @if ($leave['leave_type'] == 'full') <!-- assuming 'full' is the ID for full cuti -->
             <div>
                 <label for="number_of_leaves{{ $i }}">Jumlah Cuti {{ $i + 1 }}</label>
                 <input type="number" wire:model.defer="leaves.{{ $i }}.number_of_leaves" class="form-control" placeholder="Jumlah Cuti">
                 @error('leaves.' . $i . '.number_of_leaves') <span class="text-danger">{{ $message }}</span> @enderror
-            </div>   
+            </div>
+
             <div class="mb-3">
                 <x-form-label id="start_date{{ $i }}" label='Tanggal Mulai cuti({{ $i + 1 }})' />
                 <x-form-input type="date" id="start_date{{ $i }}" name="start_date{{ $i }}" class="form-control"
                     wire:model.defer="leaves.{{ $i }}.start_date" />
-                <small class="text-muted d-block mt-2">Perhatikan format tanggal d (Hari), m (Bulan) dan y
-                    (Tahun)</small>
+                <small class="text-muted d-block mt-2">Perhatikan format tanggal d (Hari), m (Bulan) dan y (Tahun)</small>
                 <x-form-error key="leaves.{{ $i }}.start_date" />
-            </div>          
-                     
-
-              
-
+            </div>
+            @endif
+            
+            <!-- Sisa Cuti -->
             <div>
                 <label for="remaining_days_off{{ $i }}">Sisa Cuti {{ $i + 1 }}</label>
                 <input type="number" wire:model.defer="leaves.{{ $i }}.remaining_days_off" class="form-control" placeholder="Sisa Cuti">
                 @error('leaves.' . $i . '.remaining_days_off') <span class="text-danger">{{ $message }}</span> @enderror
             </div>
 
+            <!-- Alasan Cuti -->
             <div>
                 <label for="reason{{ $i }}">Alasan Cuti {{ $i + 1 }}</label>
                 <input type="text" wire:model.defer="leaves.{{ $i }}.reason" class="form-control" placeholder="Alasan Cuti">
                 @error('leaves.' . $i . '.reason') <span class="text-danger">{{ $message }}</span> @enderror
             </div>
 
+            <!-- Status Cuti -->
             <div>
                 <label for="is_accepted{{ $i }}">Status Cuti {{ $i + 1 }}</label>
                 <select class="form-select" wire:model.defer="leaves.{{ $i }}.is_accepted">
