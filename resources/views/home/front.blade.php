9288048ca697893aea38754f8862bb5a0c19b595
Mohammad Ali <mohammad.ali1908@gmail.com>
2024-10-31 15:38:46 +0700

    ganti status jadi Hadir3

--- resources/views/home/front.blade.php
+++ resources/views/home/front.blade.php
@@ -165,7 +165,7 @@
                                                        {{ $isLate ? 'text-bg-warning' : 'text-bg-success' }}" 
                                                 data-absen-id="{{ $absen->presence_id }}"  
                                                 data-bs-toggle="modal" 
-                                                data-bs-target="#absen-detail-modal">Hadir2
+                                                data-bs-target="#absen-detail-modal">Hadir3
                                         </button>
                                     </td>
                                     @endif 
