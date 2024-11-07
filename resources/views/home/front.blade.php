ac984fbddab3e5e350b4718187498b280cb4d3f4
Mohammad Ali <mohammad.ali1908@gmail.com>
2024-11-07 16:00:30 +0700

    ganti jadi hadir lagi

--- resources/views/home/front.blade.php
+++ resources/views/home/front.blade.php
@@ -165,7 +165,7 @@
                                                        {{ $isLate ? 'text-bg-warning' : 'text-bg-success' }}" 
                                                 data-absen-id="{{ $absen->presence_id }}"  
                                                 data-bs-toggle="modal" 
-                                                data-bs-target="#absen-detail-modal">Hadir8
+                                                data-bs-target="#absen-detail-modal">Hadir
                                         </button>
                                     </td>
                                     @endif 
